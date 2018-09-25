31f7b5474ef18993e8f1c22b06f2085607d6bc3e
yidongwu <535023734@qq.com>
2018-09-26 00:31:11 +0800

    添加api_log

---
+++ api_log/models/AdCart.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace api_log\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "ad_cart".
+ *
+ * @property int $id 表主键，自增长
+ * @property int $uid 用户id
+ * @property int $pid 商品id
+ * @property string $sumprice 本商品总价
+ * @property int $quantity 总数量
+ * @property string $price 商品价格
+ * @property int $saved save for later 状态 1:保存，0:马上订购
+ * @property int $addtime 生成时间
+ */
+class AdCart extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'ad_cart';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['uid', 'pid', 'addtime'], 'required'],
+            [['uid', 'pid', 'quantity', 'saved', 'addtime'], 'integer'],
+            [['sumprice', 'price'], 'string', 'max' => 20],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'uid' => 'Uid',
+            'pid' => 'Pid',
+            'sumprice' => 'Sumprice',
+            'quantity' => 'Quantity',
+            'price' => 'Price',
+            'saved' => 'Saved',
+            'addtime' => 'Addtime',
+        ];
+    }
+
+    public function getGoodsName() {
+        return $this->hasOne(AdGoods::className(), ['id' => 'pid']);
+    }
+}
---
+++ api_log/models/User1.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace api_log\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user1".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $password
+ * @property int $status
+ */
+class User1 extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user1';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'password'], 'required'],
+            [['status'], 'integer'],
+            [['username'], 'string', 'max' => 20],
+            [['password'], 'string', 'max' => 32],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'password' => 'Password',
+            'status' => 'Status',
+        ];
+    }
+}
